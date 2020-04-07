4f1f983e9ee6f00a3aff3fef93da51517be68a3a
GrapheneCt <57592952+GrapheneCt@users.noreply.github.com>
2020-04-07 17:25:01 +0200

    Updated sample with DSP demo, added comments

--- sample/src/main.c
+++ sample/src/main.c
@@ -10,33 +10,40 @@
 
 #define CLIB_HEAP_SIZE 6 * 1024 * 1024
 
-#define SAS_THREAD_STACK 128 * 1024
+#define SAS_THREAD_STACK 128 * 1024 //Increase only if there is issues with audio playback
 #define SAS_THREAD_AFFINITY 0
-#define SAS_THREAD_PRIORITY 64
+#define SAS_THREAD_PRIORITY 64 //Must be as high as possible to avoid gaps in audio playback
 #define GRAIN 256
 
-#define PITCH 4096
-#define DRY_L_VOLUME 4096
-#define DRY_R_VOLUME 4096
-#define WET_L_VOLUME 0
-#define WET_R_VOLUME 0
-#define ADSR1 0
-#define ADSR2 0
+#define PITCH 4096 //Set pitch
+#define DRY_L_VOLUME 4096 //L channel with no effects applied
+#define DRY_R_VOLUME 4096 //R channel with no effects applied
+#define WET_L_VOLUME 4096 //L channel with effects applied
+#define WET_R_VOLUME 4096 //R channel with effects applied
+#define ADSR1 0 //Set attack-decay time
+#define ADSR2 0 //Set sustain-release time
 
 extern void* sceClibMspaceCreate(void* base, uint32_t size);
+extern void* sceClibMspaceMalloc(void* space, uint32_t size);
+extern void sceClibMspaceFree(void* space, void* ptr);
 
 int main(void)
 {
+	/* Create C heap for vitaSAS (required for system mode application support) */
+
 	void* mspace;
 	void* clibm_base;
 	SceUID clib_heap = sceKernelAllocMemBlock("ClibHeap", SCE_KERNEL_MEMBLOCK_TYPE_USER_RW_UNCACHE, CLIB_HEAP_SIZE, NULL);
 	sceKernelGetMemBlockBase(clib_heap, &clibm_base);
 	mspace = sceClibMspaceCreate(clibm_base, CLIB_HEAP_SIZE);
-
 	vitaSAS_pass_mspace(mspace);
+
+	/* Audio section */
+
 	vitaSAS_init(SCE_AUDIO_OUT_PORT_TYPE_MAIN, GRAIN, SAS_THREAD_PRIORITY, SAS_THREAD_STACK, SAS_THREAD_AFFINITY);
-	vitaSASAudio* test = vitaSAS_load_audio_VAG("app0:resource/Image.vag");
-	vitaSAS_create_voice_VAG(0, test, SCE_SAS_LOOP_ENABLE, PITCH, DRY_L_VOLUME, DRY_R_VOLUME, WET_L_VOLUME, WET_R_VOLUME, ADSR1, ADSR2);
+	vitaSASAudio* test1 = vitaSAS_load_audio_VAG("app0:resource/Image.vag");
+	vitaSAS_create_voice_VAG(0, test1, SCE_SAS_LOOP_ENABLE, PITCH, DRY_L_VOLUME, DRY_R_VOLUME, WET_L_VOLUME, WET_R_VOLUME, ADSR1, ADSR2);
+	vitaSAS_set_effect(SCE_SAS_FX_TYPE_HALL, WET_L_VOLUME, WET_R_VOLUME, 0, 0);
 	
 	sceSasSetKeyOn(0);
 
@@ -55,7 +62,7 @@ int main(void)
 		sceKernelDelayThread(10 * 1000);
 	}
 
-	vitaSAS_free_audio(test);
+	vitaSAS_free_audio(test1);
 
 	vitaSAS_finish();
 
