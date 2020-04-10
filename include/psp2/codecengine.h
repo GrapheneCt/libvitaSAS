5a39fc9ffa0e96f630c4cb4a575220520c6131ee
GrapheneCt <57592952+GrapheneCt@users.noreply.github.com>
2020-04-10 23:06:37 +0200

    Add files via upload

---
+++ include/psp2/codecengine.h
@@ -0,0 +1,43 @@
+﻿#ifndef _SCE_CODECENGINE_CODECENGINE_H
+#define _SCE_CODECENGINE_CODECENGINE_H
+
+#include <psp2/types.h>
+
+#if defined(_LANGUAGE_C_PLUS_PLUS)||defined(__cplusplus)||defined(c_plusplus)
+extern "C" {
+#endif /* defined(_LANGUAGE_C_PLUS_PLUS)||defined(__cplusplus)||defined(c_plusplus) */
+
+#define SCE_CODECENGINE_ERROR_INVALID_POINTER           (-2141192192) /* 0x80600000 */
+#define SCE_CODECENGINE_ERROR_INVALID_SIZE              (-2141192191) /* 0x80600001 */
+#define SCE_CODECENGINE_ERROR_INVALID_ALIGNMENT         (-2141192190) /* 0x80600002 */
+#define SCE_CODECENGINE_ERROR_NOT_PHYSICALLY_CONTIGUOUS (-2141192189) /* 0x80600003 */
+#define SCE_CODECENGINE_ERROR_INVALID_RANGE             (-2141192188) /* 0x80600004 */
+#define SCE_CODECENGINE_ERROR_INVALID_HEAP              (-2141192187) /* 0x80600005 */
+#define SCE_CODECENGINE_ERROR_HEAP_BROKEN               (-2141192186) /* 0x80600006 */
+#define SCE_CODECENGINE_ERROR_NO_MORE_ENTRY             (-2141192185) /* 0x80600007 */
+#define SCE_CODECENGINE_ERROR_INVALID_MEMTYPE           (-2141192184) /* 0x80600008 */
+#define SCE_CODECENGINE_ERROR_INVALID_VALUE             (-2141192183) /* 0x80600009 */
+#define SCE_CODECENGINE_ERROR_MEMORY_NOT_ALLOCATED      (-2141192182) /* 0x8060000A */
+#define SCE_CODECENGINE_ERROR_MEMORY_IN_USE             (-2141192181) /* 0x8060000B */
+#define SCE_CODECENGINE_ERROR_MEMORY_NOT_IN_USE         (-2141192180) /* 0x8060000C */
+
+extern SceUID sceCodecEngineOpenUnmapMemBlock(void *pMemBlock, SceUInt32 memBlockSize);
+extern SceInt32 sceCodecEngineCloseUnmapMemBlock(SceUID uid);
+extern SceUIntVAddr sceCodecEngineAllocMemoryFromUnmapMemBlock(SceUID uid, SceUInt32 size, SceUInt32 alignment);
+extern SceInt32 sceCodecEngineFreeMemoryFromUnmapMemBlock(SceUID uid, SceUIntVAddr p);
+
+typedef struct SceCodecEnginePmonProcessorLoad {
+	SceUInt32 size;
+	SceUInt32 average;
+} SceCodecEnginePmonProcessorLoad;
+
+extern SceInt32 sceCodecEnginePmonStart(void);
+extern SceInt32 sceCodecEnginePmonStop(void);
+extern SceInt32 sceCodecEnginePmonGetProcessorLoad(SceCodecEnginePmonProcessorLoad *pProcessorLoad);
+extern SceInt32 sceCodecEnginePmonReset(void);
+
+#if defined(_LANGUAGE_C_PLUS_PLUS)||defined(__cplusplus)||defined(c_plusplus)
+}
+#endif /* defined(_LANGUAGE_C_PLUS_PLUS)||defined(__cplusplus)||defined(c_plusplus) */
+
+#endif /* _SCE_CODECENGINE_CODECENGINE_H */
--- include/vitaSAS.h
+++ include/vitaSAS.h
@@ -5,6 +5,8 @@
 extern "C" {
 #endif
 
+#include <psp2/audiodec.h> 
+
 #define ROUND_UP(x, a)	((((unsigned int)x)+((a)-1u))&(~((a)-1u)))
 #define VITA_SAS_GRAIN_MAX 1024
 #define SCE_SAS_LOOP_DISABLE_PCM -1
@@ -14,6 +16,42 @@ typedef struct vitaSASAudio {
 	size_t data_size;
 	SceUID data_id;
 } vitaSASAudio;
+
+typedef struct File {
+	const char *pName;
+	uint32_t size;
+} File;
+
+typedef struct Buffer {
+	uint8_t *p;
+	uint8_t *op[2];
+	uint32_t size;
+	uint32_t offsetR;
+	uint32_t offsetW;
+	uint32_t bufIndex;
+} Buffer;
+
+typedef struct FileStream {
+	File file;
+	Buffer buf;
+} FileStream;
+
+typedef struct CodecEngineMemBlock {
+	SceUID uidMemBlock;
+	SceUID uidUnmap;
+	unsigned int vaContext;
+	unsigned int contextSize;
+} CodecEngineMemBlock;
+
+typedef struct VitaSAS_Decoder {
+	FileStream* pInput;
+	FileStream* pOutput;
+	SceAudiodecCtrl* pAudiodecCtrl;
+	SceAudiodecInfo* pAudiodecInfo;
+	CodecEngineMemBlock* codecMemBlock;
+	unsigned int headerSize;
+	unsigned int decodeStatus;
+} VitaSAS_Decoder;
 
 /* Common */
 
@@ -25,21 +63,49 @@ void vitaSAS_free_audio(vitaSASAudio* info);
 vitaSASAudio* vitaSAS_load_audio_VAG(const char* soundPath);
 vitaSASAudio* vitaSAS_load_audio_PCM(const char* soundPath);
 
+/* Codec Engine decoding */
+
+void vitaSAS_destroy_decoder(VitaSAS_Decoder* decoderInfo);
+VitaSAS_Decoder* vitaSAS_create_AT9_decoder(const char* soundPath);
+//VitaSAS_Decoder* vitaSAS_create_MP3_decoder(const char* soundPath);
+VitaSAS_Decoder* vitaSAS_create_AAC_decoder(const char* soundPath);
+void vitaSAS_decoder_start_playback(VitaSAS_Decoder* decoderInfo, unsigned int thPriority, unsigned int thStackSize, unsigned int thCpu);
+void vitaSAS_decoder_pause_playback(VitaSAS_Decoder* decoderInfo);
+void vitaSAS_decoder_resume_playback(VitaSAS_Decoder* decoderInfo);
+void vitaSAS_decoder_stop_playback(VitaSAS_Decoder* decoderInfo);
+void vitaSAS_decoder_seek(VitaSAS_Decoder* decoderInfo, unsigned int nEsSamples);
+void vitaSAS_decode_to_buffer(VitaSAS_Decoder* decoderInfo, unsigned int begEsSamples, unsigned int nEsSamples, uint8_t* buffer);
+unsigned int vitaSAS_decoder_get_current_es_offset(VitaSAS_Decoder* decoderInfo);
+
+
 /* Voices */
 
-void vitaSAS_create_voice_VAG(unsigned int voiceID, const vitaSASAudio* info,
-	unsigned int loop, unsigned int pitch, unsigned int volRDry, unsigned int volLDry,
-	unsigned int volRWet, unsigned int volLWet, unsigned int adsr1, unsigned int adsr2);
+void vitaSAS_set_voice_VAG(unsigned int voiceID, const vitaSASAudio* info,
+	unsigned int loop, unsigned int pitch, unsigned int volLDry, unsigned int volRDry,
+	unsigned int volLWet, unsigned int volRWet, unsigned int adsr1, unsigned int adsr2);
 
-void vitaSAS_create_voice_PCM(unsigned int voiceID, const vitaSASAudio* info,
-	unsigned int loop, unsigned int pitch, unsigned int volRDry, unsigned int volLDry,
-	unsigned int volRWet, unsigned int volLWet, unsigned int adsr1, unsigned int adsr2);
+void vitaSAS_set_voice_PCM(unsigned int voiceID, const vitaSASAudio* info,
+	unsigned int loopSize, unsigned int pitch, unsigned int volLDry, unsigned int volRDry,
+	unsigned int volLWet, unsigned int volRWet, unsigned int adsr1, unsigned int adsr2);
 
-void vitaSAS_create_voice_noise(unsigned int voiceID, unsigned int clock, unsigned int pitch, unsigned int volRDry,
-	unsigned int volLDry, unsigned int volRWet, unsigned int volLWet, unsigned int adsr1, unsigned int adsr2);
+void vitaSAS_set_voice_noise(unsigned int voiceID, unsigned int clock, unsigned int pitch, unsigned int volLDry,
+	unsigned int volRDry, unsigned int volLWet, unsigned int volRWet, unsigned int adsr1, unsigned int adsr2);
+
+/* Effects */
 
 void vitaSAS_set_effect(unsigned int effectType, unsigned int volL, unsigned int volR, unsigned int delayTime, unsigned int feedbackLevel);
 void vitaSAS_reset_effect(void);
+
+/* Internal functions */
+
+void vitaSAS_internal_set_initial_params(unsigned int voiceID, unsigned int pitch, unsigned int volLDry,
+	unsigned int volRDry, unsigned int volLWet, unsigned int volRWet, unsigned int adsr1, unsigned int adsr2);
+
+CodecEngineMemBlock* vitaSAS_internal_allocate_memory_for_codec_engine(unsigned int codecType, SceAudiodecCtrl* addecctrl);
+void vitaSAS_internal_free_memory_for_codec_engine(const CodecEngineMemBlock* codecMemBlock);
+void vitaSAS_internal_output_for_decoder(Buffer *pOutput);
+int vitaSAS_internal_getFileSize(const char *pInputFileName, uint32_t *pInputFileSize);
+int vitaSAS_internal_readFile(const char *pInputFileName, void *pInputBuf, uint32_t inputFileSize);
 
 #ifdef __cplusplus
 }
